a206704cae40169485ae2388f251554f4e6ddd4a
akshaylals <akshaylal.s@inapp.com>
2022-09-20 11:44:47 +0530

    update products endpoint

--- ERP/Constants.cs
+++ ERP/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public static string productsEndpoint = "https://fakestoreapi.com/products";
+        public static string productsEndpoint = "https://192.168.2.65:3000/products";
         public static string cartEndpoint = "http://192.168.2.65:3000/cart";
     }
 }
