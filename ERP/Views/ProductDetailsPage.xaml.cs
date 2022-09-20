57add0e77212049bf4c4ae775930186d36a0c1c2
akshaylals <akshaylal.s@inapp.com>
2022-09-20 15:59:57 +0530

    product view model fix

--- ERP/Views/ProductDetailsPage.xaml.cs
+++ ERP/Views/ProductDetailsPage.xaml.cs
@@ -1,12 +1,26 @@
+using ERP.Models;
 using ERP.ViewModels;
 
 namespace ERP.Views;
 
+[QueryProperty(nameof(Vm), "viewModel")]
 public partial class ProductDetailsPage : ContentPage
 {
-	public ProductDetailsPage(ProductDetailsViewModel viewModel)
+	ProductDetailsViewModel viewModel;
+	public ProductDetailsViewModel Vm
+	{
+		get => viewModel;
+		set
+		{
+			viewModel = value;
+			OnPropertyChanged();
+		}
+
+    }
+	
+	public ProductDetailsPage()
 	{
 		InitializeComponent();
-		BindingContext = viewModel;
+		BindingContext = this;
 	}
 }
