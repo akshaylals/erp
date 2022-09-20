57c36c1298a10cae9c2c9900fcc216b0d688cdda
akshaylals <akshaylal.s@inapp.com>
2022-09-20 11:48:21 +0530

    added cartitem model

---
+++ ERP/Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ERP.Models
+{
+    internal class CartItem
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("product")]
+        public Product Product { get; set; }
+
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+    }
+}
